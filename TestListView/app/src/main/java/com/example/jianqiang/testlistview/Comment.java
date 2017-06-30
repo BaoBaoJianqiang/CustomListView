a8de84db93f050cd89f23541f5e1530e083cebd8
pangchao <15527283152@163.com>
2017-06-30 11:01:39 +0800

    测试git

--- TestListView/app/src/main/java/com/example/jianqiang/testlistview/Comment.java
+++ TestListView/app/src/main/java/com/example/jianqiang/testlistview/Comment.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Comment {
     public String author;
     public String content;
+    public String ceshi ;
 
     public Comment(String author, String content) {
         this.author = author;
