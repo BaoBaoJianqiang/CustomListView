f2169e89e218b03ee8f9733c75e7650d11ddf9b7
struggle_li <liping429873911@163.com>
2017-07-04 20:46:49 +0800

    修改分页问题去掉view

--- TestListViewNew/app/src/main/java/com/example/jianqiang/testlistview/Utils.java
+++ TestListViewNew/app/src/main/java/com/example/jianqiang/testlistview/Utils.java
@@ -13,16 +13,32 @@ public class Utils {
     public static List<News> generateData() {
         List<News> newsList = new ArrayList<News>();
 
-        newsList.add(genNew1());
-        newsList.add(genNew2());
-        newsList.add(genNew3());
-        newsList.add(genNew4());
-        newsList.add(genNew5());
-        newsList.add(genNew6());
-        newsList.add(genNew7());
-        newsList.add(genNew8());
-        newsList.add(genNew9());
-        newsList.add(genNew10());
+        newsList.add(genNew1(0));
+        newsList.add(genNew2(0));
+        newsList.add(genNew3(0));
+        newsList.add(genNew4(0));
+        newsList.add(genNew5(0));
+        newsList.add(genNew6(0));
+        newsList.add(genNew7(0));
+        newsList.add(genNew8(0));
+        newsList.add(genNew9(0));
+
+        return newsList;
+    }
+
+
+    public static List<News> generateLoadData(int hours) {
+        List<News> newsList = new ArrayList<News>();
+
+        newsList.add(genNew1(hours));
+        newsList.add(genNew2(hours));
+        newsList.add(genNew3(hours));
+        newsList.add(genNew4(hours));
+        newsList.add(genNew5(hours));
+        newsList.add(genNew6(hours));
+        newsList.add(genNew7(hours));
+        newsList.add(genNew8(hours));
+        newsList.add(genNew9(hours));
 
         return newsList;
     }
@@ -52,28 +68,28 @@ public class Utils {
         return staticLayout;
     }
 
-    static News genNew1() {
+    static News genNew1(int hours) {
         List<Comment> commentList1 = new ArrayList<Comment>();
         commentList1.add(new Comment("希区柯克", "有你真好"));
         commentList1.add(new Comment("小鱼儿 回复 希区柯克", "我是天才"));
         commentList1.add(new Comment("包建强", "咸鱼翻身！"));
-
+        hours += 1;
         News news1 = new News();
         news1.author = "小鱼儿";
         news1.avator = "https://pic.cnblogs.com/face/313471/20141107134952.png";
         news1.content = "毕业蓝翔技校，技能搬运工！专门搜集生活中的实用小技能、小智慧，为您的生活提供更多帮助与便利。";
-        news1.showtime = "1小时前";
+        news1.showtime = hours+"小时前";
         news1.commentList = commentList1;
 
         return news1;
     }
 
-    static News genNew2() {
+    static News genNew2(int hours) {
         List<Comment> commentList1 = new ArrayList<Comment>();
         commentList1.add(new Comment("阿轲", "背后攻击加倍伤害"));
         commentList1.add(new Comment("孙尚香 回复 阿轲", "来一个肉"));
         commentList1.add(new Comment("太乙真人", "我游走！"));
-
+        hours += 2;
         List<ImageEntity> imageList = new ArrayList<>();
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195629789-2082196185.jpg",
@@ -83,7 +99,7 @@ public class Utils {
         news1.author = "太乙真人";
         news1.avator = "https://pic.cnblogs.com/face/605230/20170228090302.png";
         news1.content = "王者荣耀走起";
-        news1.showtime = "2小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"高渐离", "张飞", "不知火舞"};
         news1.commentList = commentList1;
         news1.imageList = imageList;
@@ -91,12 +107,12 @@ public class Utils {
         return news1;
     }
 
-    static News genNew3() {
+    static News genNew3(int hours) {
         List<Comment> commentList1 = new ArrayList<Comment>();
         commentList1.add(new Comment("李四", "80分走起"));
         commentList1.add(new Comment("石头回复李四", "避风塘吧"));
         commentList1.add(new Comment("王二麻子", "谁跟我对家？"));
-
+        hours += 3;
         List<ImageEntity> imageList = new ArrayList<>();
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195630930-225242378.jpg",
@@ -109,7 +125,7 @@ public class Utils {
         news1.author = "张三";
         news1.avator = "https://pic.cnblogs.com/face/u383503.jpg?id=26222424";
         news1.content = "晚上团建";
-        news1.showtime = "3小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"老爸", "老妈", "三姨"};
         news1.commentList = commentList1;
         news1.imageList = imageList;
@@ -117,7 +133,7 @@ public class Utils {
         return news1;
     }
 
-    static News genNew4() {
+    static News genNew4(int hours) {
         List<ImageEntity> imageList = new ArrayList<>();
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195634383-1070913148.jpg",
@@ -128,19 +144,19 @@ public class Utils {
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195636602-372270222.jpg",
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195636274-587297471.jpg"));
-
+        hours += 4;
         News news1 = new News();
         news1.author = "肉胖";
         news1.avator = "https://pic.cnblogs.com/face/433855/20130219085148.png";
         news1.content = "TCP/IP协议栈是一系列网络协议的总和，是构成网络通信的核心骨架";
-        news1.showtime = "4小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"叶曲波", "起为", "郭刚"};
         news1.imageList = imageList;
 
         return news1;
     }
 
-    static News genNew5() {
+    static News genNew5(int hours) {
         List<Comment> commentList1 = new ArrayList<Comment>();
         commentList1.add(new Comment("巴尔扎克", "写得不错"));
         commentList1.add(new Comment("雨果 回复 巴尔扎克", "送一套人间喜剧"));
@@ -159,12 +175,12 @@ public class Utils {
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195642618-1809860382.jpg",
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195642149-516368770.jpg"));
-
+        hours += 5;
         News news1 = new News();
         news1.author = "萨克雷";
         news1.avator = "https://pic.cnblogs.com/face/834468/20151115214952.png";
         news1.content = "名利场终于写完了";
-        news1.showtime = "5小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"高尔斯华绥", "阿婆", "乔叟"};
         news1.commentList = commentList1;
         news1.imageList = imageList;
@@ -172,12 +188,12 @@ public class Utils {
         return news1;
     }
 
-    static News genNew6() {
+    static News genNew6(int hours) {
         List<Comment> commentList1 = new ArrayList<Comment>();
         commentList1.add(new Comment("小红帽", "蛋糕做得不错"));
         commentList1.add(new Comment("包建强 回复 小红帽", "谢谢夸奖"));
         commentList1.add(new Comment("大灰狼", "提拉米苏有吗？"));
-
+        hours += 6;
         List<ImageEntity> imageList = new ArrayList<>();
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195643711-1422992067.jpg",
@@ -194,12 +210,11 @@ public class Utils {
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170702195642618-1809860382.jpg",
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170704170659737-1337992150.jpg"));
-
         News news1 = new News();
         news1.author = "包建强";
         news1.avator = "https://pic.cnblogs.com/face/1118933/20170616222517.png";
         news1.content = "晒一下我的烘焙蛋糕";
-        news1.showtime = "6小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"蛋糕达人", "白雪公主", "蓝精灵"};
         news1.commentList = commentList1;
         news1.imageList = imageList;
@@ -208,12 +223,12 @@ public class Utils {
     }
 
 
-    static News genNew7() {
+    static News genNew7(int hours) {
         List<Comment> commentList1 = new ArrayList<Comment>();
         commentList1.add(new Comment("杨逍", "忍着"));
         commentList1.add(new Comment("张无忌 回复 杨逍", "。。。"));
         commentList1.add(new Comment("朱元璋", "有痔疮贴肛泰！"));
-
+        hours += 7;
         List<ImageEntity> imageList = new ArrayList<>();
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170704170704847-1486041241.jpg",
@@ -238,7 +253,7 @@ public class Utils {
         news1.author = "张无忌";
         news1.avator = "https://pic.cnblogs.com/face/250417/20160816172634.png";
         news1.content = "有痔疮怎么办？";
-        news1.showtime = "7小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"周芷若", "赵敏", "小昭"};
         news1.commentList = commentList1;
         news1.imageList = imageList;
@@ -246,12 +261,12 @@ public class Utils {
         return news1;
     }
 
-    static News genNew8() {
+    static News genNew8(int hours) {
         List<Comment> commentList1 = new ArrayList<Comment>();
         commentList1.add(new Comment("李逵", "我想娶老婆！"));
         commentList1.add(new Comment("宋江 回复 李逵", "忍着！"));
         commentList1.add(new Comment("萧让", "我来写！"));
-
+        hours += 8;
         List<ImageEntity> imageList = new ArrayList<>();
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170704170715925-685466831.jpg",
@@ -279,7 +294,7 @@ public class Utils {
         news1.author = "宋江";
         news1.avator = "https://pic.cnblogs.com/face/1181291/20170629150910.png";
         news1.content = "105个男人和3个女人的故事";
-        news1.showtime = "8小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"李逵", "卢俊义", "孙二娘"};
         news1.commentList = commentList1;
         news1.imageList = imageList;
@@ -287,7 +302,7 @@ public class Utils {
         return news1;
     }
 
-    static News genNew9() {
+    static News genNew9(int hours) {
         List<Comment> commentList1 = new ArrayList<Comment>();
         commentList1.add(new Comment("孙猴", "吃俺老孙一棒！"));
         commentList1.add(new Comment("老猪", "高老庄一日游"));
@@ -318,12 +333,12 @@ public class Utils {
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170704170748565-628161064.jpg",
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170704170747237-1890139313.jpg"));
-
+        hours += 9;
         News news1 = new News();
         news1.author = "唐僧";
         news1.avator = "https://pic.cnblogs.com/face/601535/20140621193541.png";
         news1.content = "骑白马的不一定是我～～";
-        news1.showtime = "9小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"杨戬", "太上老君的那头牛", "玉皇大帝"};
         news1.commentList = commentList1;
         news1.imageList = imageList;
@@ -331,7 +346,7 @@ public class Utils {
         return news1;
     }
 
-    static News genNew10() {
+    static News genNew10(int hours) {
         List<ImageEntity> imageList = new ArrayList<>();
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170704170749815-195041392.jpg",
@@ -360,12 +375,12 @@ public class Utils {
         imageList.add(new ImageEntity(
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170704170804284-1909970115.jpg",
                 "http://images2015.cnblogs.com/blog/13430/201707/13430-20170704170803378-885659992.jpg"));
-
+        hours += 10;
         News news1 = new News();
         news1.author = "包建强";
         news1.avator = "https://pic.cnblogs.com/face/1118933/20170616222517.png";
         news1.content = "Android培训哪家强？";
-        news1.showtime = "10小时前";
+        news1.showtime = hours+"小时前";
         news1.preferList = new String[]{"老邓", "老张", "玉刚"};
         news1.imageList = imageList;
 
