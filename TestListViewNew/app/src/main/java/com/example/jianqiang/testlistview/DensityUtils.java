b89ea250405effcdc73acc1ca295299beff7a760
pangchao <15527283152@163.com>
2017-07-06 18:03:35 +0800

    1.在资源文件中定义尺寸，适配多分辨率 2.加入线程池，在子线程中进行高度的计算 3.优化界面排版

---
+++ TestListViewNew/app/src/main/java/com/example/jianqiang/testlistview/DensityUtils.java
@@ -0,0 +1,24 @@
+package com.example.jianqiang.testlistview;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.DimenRes;
+import android.util.TypedValue;
+
+public class DensityUtils {
+
+    public static int dp2px(Context ctx, float dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, ctx.getResources().getDisplayMetrics());
+
+    }
+
+    public static int px2dp(Context ctx, int px) {
+        float density = ctx.getResources().getDisplayMetrics().density;
+        return (int) (px / density + 0.5f);
+    }
+
+    public static int getDimens(Context context, @DimenRes int resource) {
+        Resources mResources = context.getResources();
+        return mResources.getDimensionPixelSize(resource);
+    }
+}
