8b9c3d83869eb8edf24976aa6036aa32d4743bd0
pangchao <15527283152@163.com>
2017-07-06 18:31:24 +0800

    再次提交 1.在资源文件中定义尺寸，适配多分辨率 2.加入线程池，在子线程中进行高度的计算 3.优化界面排版

--- TestListViewNew/app/src/main/java/com/example/jianqiang/testlistview/helpers/ItemViewLayoutConfig.java
+++ TestListViewNew/app/src/main/java/com/example/jianqiang/testlistview/helpers/ItemViewLayoutConfig.java
@@ -1,93 +1,122 @@
 package com.example.jianqiang.testlistview.helpers;
 
 import android.content.Context;
-import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 
 import com.example.jianqiang.testlistview.R;
 
 import java.io.Serializable;
 
-public class ItemViewLayoutConfig implements Serializable
-{
+public class ItemViewLayoutConfig implements Serializable {
+
+
     private ConfigBuilder builder;
-    private Context context;
+    private Context mContext;
 
-    public ItemViewLayoutConfig(ConfigBuilder builder)
-    {
+    public ConfigBuilder getBuilder() {
+        return builder;
+    }
+
+    public ItemViewLayoutConfig(ConfigBuilder builder) {
         this.builder = builder;
-
-        this.context = builder.context;
+        this.mContext = builder.context;
     }
 
-    public float getPrimaryTextSize()
-    {
-        return builder.primaryTextSize > 0 ? builder.primaryTextSize : context.getResources().getDimension(R.dimen.common_text_size_medium);
-    }
 
-    public float getSecondaryTextSize()
-    {
-        return builder.secondaryTextSize > 0 ? builder.secondaryTextSize : context.getResources().getDimension(R.dimen.common_text_size_small);
-    }
-
-    @ColorInt
-    public int getPrimaryTextColor()
-    {
-        return builder.primaryTextColor == 0 ? context.getResources().getColor(R.color.common_text_color_primary) : builder.primaryTextColor;
-    }
-
-    @ColorInt
-    public int getSecondaryTextColor()
-    {
-        return builder.secondaryTextColor == 0 ? context.getResources().getColor(R.color.common_text_color_secondary) : builder.secondaryTextColor;
-    }
-
-    public static class ConfigBuilder
-    {
+    public static class ConfigBuilder {
         Context context;
-        int height;
-        int width;
-        float primaryTextSize;
-        float secondaryTextSize;
-        @ColorInt int primaryTextColor;
-        @ColorInt int secondaryTextColor;
+        int nameColor;//文字颜色
+        int timeColor;//状态发布时间颜色
+        int contentColor;//内容颜色
+        private int rightMargin;//item右边距
+        private float nameSize;//名字的颜色大小
+        private float contentSize;//发表内容文字大小
+        private int lineMargin;//行间距
+        private int imageSpace;//图片间距
 
-        public ConfigBuilder(@NonNull Context context)
-        {
+
+        private int imageSize;//分享的图片尺寸
+        private int contentWidth;//item减去图片以及右间距
+        private int articleHeight;//分享文章的高度
+        private int articleFontSize;//分享文章的文字的大小
+
+
+        private void initViewSize() {
+            nameColor = ContextCompat.getColor(context, R.color.blue);
+            timeColor = ContextCompat.getColor(context, R.color.gray);
+            contentColor = ContextCompat.getColor(context, R.color.black_text);
+            rightMargin = (int) context.getResources().getDimension(R.dimen.dp_10);
+            imageSize = (int) context.getResources().getDimension(R.dimen.dp_50);
+            nameSize = context.getResources().getDimension(R.dimen.sp_10);
+            contentSize = context.getResources().getDimension(R.dimen.sp_11);
+            lineMargin = (int) context.getResources().getDimension(R.dimen.dp_3);
+            imageSpace = (int) context.getResources().getDimension(R.dimen.dp_3);
+            contentWidth = (int) context.getResources().getDimension(R.dimen.dp_167);
+            articleHeight = (int) context.getResources().getDimension(R.dimen.dp_20);
+            articleFontSize = (int) context.getResources().getDimension(R.dimen.sp_11);
+        }
+
+        public ConfigBuilder(@NonNull Context context) {
             this.context = context;
+            initViewSize();
         }
 
-        public ConfigBuilder primaryTextSize(float primaryTextSize)
-        {
-            this.primaryTextSize = primaryTextSize;
-
-            return this;
-        }
-
-        public ConfigBuilder secondaryTextSize(float secondaryTextSize)
-        {
-            this.secondaryTextSize = secondaryTextSize;
-
-            return this;
-        }
-
-        public ConfigBuilder primaryTextColor(@ColorInt int primaryTextColor)
-        {
-            this.primaryTextColor = primaryTextColor;
-
-            return this;
-        }
-
-        public ConfigBuilder secondaryTextColor(@ColorInt int secondaryTextColor)
-        {
-            this.secondaryTextColor = secondaryTextColor;
-
-            return this;
-        }
-
-        public ItemViewLayoutConfig build()
-        {
+        public ItemViewLayoutConfig build() {
             return new ItemViewLayoutConfig(this);
         }
+
+        public int getImageSize() {
+            return imageSize;
+        }
+
+        public int getrightMargin() {
+            return rightMargin;
+        }
+
+        public float getNameSize() {
+            return nameSize;
+        }
+
+        public float getContentSize() {
+            return contentSize;
+        }
+
+        public int getLineMargin() {
+            return lineMargin;
+        }
+
+        public int getImageSpace() {
+            return imageSpace;
+        }
+
+        public int getContentWidth() {
+            return contentWidth;
+        }
+
+        public int getArticleHeight() {
+            return articleHeight;
+        }
+
+        public int getArticleFontSize() {
+            return articleFontSize;
+        }
+
+        public int getnameColor() {
+            return nameColor;
+        }
+
+        public int gettimeColor() {
+            return timeColor;
+        }
+
+        public int getContentColor() {
+            return contentColor;
+        }
+
+        public void setContentColor(int contentColor) {
+            this.contentColor = contentColor;
+        }
+
     }
 }
